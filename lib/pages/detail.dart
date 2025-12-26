1122c556ddfa87579a5e07e7d80319f4cdd0f761
minthantko <minthantko101@gmail.com>
2025-12-26 10:36:34 +0700

    update detail page

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
-  const DetailPage({super.key});
+  final v1, v2, v3, v4;
+  const DetailPage(this.v1, this.v2, this.v3, this.v4);
 
   @override
   State<DetailPage> createState() => _DetailPageState();
 }
 
 class _DetailPageState extends State<DetailPage> {
+  var _v1, _v2, _v3, _v4;
+  @override
+  void initState() {
+    super.initState();
+    _v1 = widget.v1;
+    _v2 = widget.v2;
+    _v3 = widget.v3;
+    _v4 = widget.v4;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Detail Page")),
-      body: ListView(
-        children: [
-          Text(
-            "This is Detail Page",
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [Text(_v1), Text(_v2), Image.network(_v3), Text(_v4)],
+        ),
       ),
     );
   }
